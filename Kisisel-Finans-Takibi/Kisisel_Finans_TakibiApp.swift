2bf043c71787bfe63b57146cba606451c7c6ba22
Ejder Dag <ejdrdag@gmail.com>
2025-03-31 18:58:56 +0300

    Initial Commit

---
+++ Kisisel-Finans-Takibi/Kisisel_Finans_TakibiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kisisel_Finans_TakibiApp.swift
+//  Kisisel-Finans-Takibi
+//
+//  Created by Ejder Dağ on 31.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Kisisel_Finans_TakibiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
